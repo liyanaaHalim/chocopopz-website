af6cda12682d489c9da10e2dde12570331d7ef91
liyanaaHalim <nurliyanaa983@gmail.com>
2025-05-20 00:48:06 +0800

    Initial website upload

---
+++ src/test/java/uum/chocopopz/chocopopzwebsite/ChocopopzWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package uum.chocopopz.chocopopzwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocopopzWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
